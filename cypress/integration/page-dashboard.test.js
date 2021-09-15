0cf31dee6cba831be6512b901345197b18c06638
MIke Witk <mike.witk@gmail.com>
2021-09-15 08:25:10 -0400

    test[CY]: dashboard page should display

---
+++ cypress/integration/page-dashboard.test.js
@@ -0,0 +1,27 @@
+describe('Dashboard Page', () => {
+  it('should display company numbers summary', () => {
+    const totalRevenue = '$123,456.78';
+    const totalTransactions = '123,456';
+    const totalProducts = '123,456';
+    const totalUsers = '123,456';
+
+    cy.server({ force404: true });
+
+    cy.route({
+      method: 'GET',
+      url: 'https://my.api.mockaroo.com/summary.json?key=c7b1dcd0',
+      response: {
+        total_revenue: 236345.67,
+        total_transactions: 7714,
+        total_products: 14163,
+        total_users: 17120,
+      },
+    });
+
+    cy.visit('/dashboard');
+    cy.contains(totalRevenue);
+    cy.contains(totalTransactions);
+    cy.contains(totalProducts);
+    cy.contains(totalUsers);
+  });
+});
--- cypress/integration/smoke.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Smoke test', () => {
-  it('should view the home page', () => {
-    cy.visit('/');
-    cy.contains('Learn React');
-  });
-});
