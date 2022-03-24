c10003f89c25b8010af2dd051fd2075d4acd3a98
MIke Witk <mike.witk@gmail.com>
2022-03-24 19:45:02 -0400

    add: react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+render(
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById('root'),
 );
