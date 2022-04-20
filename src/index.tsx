617a18a2b7360e8d5fa6629276ef0a478dbe5a2e
MIke Witk <mike.witk@gmail.com>
2022-04-19 21:07:57 -0400

    add: react query instance

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import App from './App';
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-		</Routes>
-	</BrowserRouter>,
+	<QueryClientProvider client={queryClient}>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</QueryClientProvider>,
 	document.getElementById('root'),
 );
